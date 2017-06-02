d19b49c3db9a4e6ba06bebfcc7216d974e7e7771
Gustavo Verzola <verzola@gmail.com>
2017-06-02 16:30:49 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,7 @@ sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl https://raw.githubusercontent.com/verzola/aliases/master/install.sh | bash
 git clone https://github.com/verzola/.zshrc.git ~/projects/zshrc
-ln -s ~/projects/zshrc/.zshrc ~/.zshrc
+if [ ! -f ~/.zshrc ]
+then
+    ln -s ~/projects/zshrc/.zshrc ~/.zshrc
+fi
