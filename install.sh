f6c04e7086534a27095b423cd56df2f39b21d606
Gustavo Verzola <verzola@gmail.com>
2019-08-30 15:49:51 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -e
 
-mkdir -p ~/projects/verzola/
+mkdir -p ~/projects/verzola
+
+if [ ! -f ~/.zshrc ]; then
+    ln -s ~/projects/verzola/zshrc/.zshrc ~/.zshrc
+fi
 
 if [ -d ~/.oh-my-zsh ]; then
   echo "Updating Oh My Zsh..."
@@ -15,8 +19,4 @@ if [ ! -d ~/projects/verzola/zshrc ]; then
   git clone https://github.com/verzola/.zshrc.git ~/projects/verzola/zshrc
 else
   git -C ~/projects/verzola/zshrc pull origin master
-fi
-
-if [ ! -f ~/.zshrc ]; then
-    ln -s ~/projects/verzola/zshrc/.zshrc ~/.zshrc
 fi
