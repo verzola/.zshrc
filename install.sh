e5dba633827a0e50405227d272d9e042e5f4b225
Gustavo Verzola <verzola@gmail.com>
2019-08-30 15:08:06 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
-sudo apt install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-curl https://raw.githubusercontent.com/verzola/aliases/master/install.sh | bash
-git clone https://github.com/verzola/.zshrc.git ~/projects/zshrc
-if [ ! -f ~/.zshrc ]
-then
-    ln -s ~/projects/zshrc/.zshrc ~/.zshrc
+#!/bin/sh
+set -e
+
+if [ -d ~/.oh-my-zsh ]; then
+  echo "Updating Oh My Zsh..."
+  zsh -ic "upgrade_oh_my_zsh"
+else
+  echo "Installing Oh My Zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+if [ ! -d ~/projects/verzola/zshrc ]; then
+  git clone https://github.com/verzola/.zshrc.git ~/projects/verzola/zshrc
+else
+  git -C ~/projects/verzola/zshrc pull origin master
+fi
+
+if [ ! -f ~/.zshrc ]; then
+    ln -s ~/projects/verzola/zshrc/.zshrc ~/.zshrc
 fi
